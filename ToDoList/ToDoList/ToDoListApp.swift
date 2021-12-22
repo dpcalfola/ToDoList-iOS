3a87ca1028749b2a7e94627897bffb713c9851aa
Fola Flor <dpcalfola@gmail.com>
2021-12-22 16:29:01 +0900

    ver 0.01
    Buildup basic structure

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Fola Flor on 2021/12/22.
+//
+
+import SwiftUI
+
+/*
+ 
+ MVVM Architecture
+
+ Moded - data point
+ Viwe - UI
+ ViewModel - manages Models
+
+ */
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
